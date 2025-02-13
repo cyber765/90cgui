639006a2d09e70ef032674c9e7ea9b0ac14dec8b
cyber765 <112839510+cyber765@users.noreply.github.com>
2025-02-13 13:20:36 +0800

    Delete torch directory

--- torch/version.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-__all__ = ['__version__', 'debug', 'cuda', 'git_version', 'hip']
-__version__ = '2.5.1'
-debug = False
-cuda: Optional[str] = None
-git_version = 'Unknown'
-hip: Optional[str] = None
